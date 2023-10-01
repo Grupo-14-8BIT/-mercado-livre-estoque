ce074c26498a8561473bd34157ab1315012e54e5
ARTHUR BUSANELLO <tuti.busanello@hotmail.com>
2023-10-01 00:52:07 -0300

    teste1

--- src/main/java/com/stock/stock/StockApplication.java
+++ src/main/java/com/stock/stock/StockApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockApplication {
-
+//aaaaaa
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
 	}
