a56dd7c889df8551d05c260cfd3077d65e41f256
unknown <josesilveira4152@gmail.com>
2023-10-02 00:41:34 -0300

    sku funcionando

--- src/main/java/com/stock/stock/Controller/SkuController.java
+++ src/main/java/com/stock/stock/Controller/SkuController.java
@@ -1,6 +1,11 @@
 package com.stock.stock.Controller;
 
 import com.stock.stock.Service.SkuSimplesService;
+import com.stock.stock.entity.Anuncio;
+import com.stock.stock.entity.SkuSimples;
+import com.stock.stock.entity.SkuSimplesDTO;
+import com.stock.stock.repository.AnuncioRepository;
+import com.stock.stock.repository.SkuSimplesRepository;
 import com.stock.stock.user.User;
 import com.stock.stock.user.UserRepository;
 import com.stock.stock.user.config.JwtService;
@@ -8,11 +13,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.badRequest;
 
 @RestController
 @RequestMapping("/api/v1/admin/sku")
@@ -34,6 +39,13 @@ public class SkuController {
     @Autowired
     private SkuSimplesService service;
 
+    @Autowired
+    private SkuSimplesRepository repository;
+
+
+    @Autowired
+    private AnuncioRepository anuncioRepository;
+
     @GetMapping("/fetch")
     public ResponseEntity<String> fetch( HttpServletRequest request) {
 
@@ -44,8 +56,56 @@ public class SkuController {
         userEmail = jwtService.extractUsername(jwt);
         Optional<User> user = userRepository.findByEmail(userEmail);
 
+
+
       return    service.fetch(user.get());
 
+
+
+    };
+
+
+    @GetMapping("/change")
+    public ResponseEntity<String> mudarSkuAnuncio(
+            HttpServletRequest request,@RequestParam String mlb, @RequestParam String sku
+    ) {
+
+        final String userEmail;
+        final String jwt;
+        final String authHeader = request.getHeader("Authorization");
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        Optional<User> user = userRepository.findByEmail(userEmail);
+
+        Optional<Anuncio> anuncio = anuncioRepository.findAnuncioByMlb(mlb);
+       if ( anuncio.get().getConta().getUsuario() == user.get() ) {
+           return service.mudarSkuAnuncio(anuncio.get(),sku,anuncio.get().getConta());
+       } else {return badRequest().body(" Usuario nao autorizado");}
+
+    };
+
+
+
+    @PutMapping("/update")
+    public ResponseEntity<String> update(
+            HttpServletRequest request, @RequestParam String sku,
+            @RequestBody SkuSimplesDTO skuSimplesDTO
+            ) {
+
+        final String userEmail;
+        final String jwt;
+        final String authHeader = request.getHeader("Authorization");
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        Optional<User> user = userRepository.findByEmail(userEmail);
+
+        Optional<SkuSimples> skuSimplesOptional = repository.findBySKU(sku);
+
+        if ( skuSimplesOptional.isPresent() && skuSimplesOptional.get().getUser() == user.get()) {
+        return service.update(skuSimplesDTO,sku);
+        } else {
+            return  badRequest().body("Sku nao existe na sua conta");
+        }
 
 
     };
--- src/main/java/com/stock/stock/repository/AnuncioRepository.java
+++ src/main/java/com/stock/stock/repository/AnuncioRepository.java
@@ -1,9 +1,11 @@
 package com.stock.stock.repository;
 
 import com.stock.stock.entity.Anuncio;
+import com.stock.stock.entity.SkuSimples;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,6 +13,8 @@ public interface AnuncioRepository extends JpaRepository <Anuncio, Integer> {
 
 
     Optional<Anuncio> findAnuncioByMlb(String mlb);
+
+    List<Anuncio> findAllBySkuSimples(SkuSimples skuSimples);
 
 
 }
