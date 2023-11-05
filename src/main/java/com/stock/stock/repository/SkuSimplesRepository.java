eaf341b4bec8ca3594c609030fa76e3ea3ae6b5c
unknown <josesilveira4152@gmail.com>
2023-11-05 00:48:36 -0300

    getAll sku

--- src/main/java/com/stock/stock/repository/SkuSimplesRepository.java
+++ src/main/java/com/stock/stock/repository/SkuSimplesRepository.java
@@ -1,14 +1,17 @@
 package com.stock.stock.repository;
 
 import com.stock.stock.entity.SkuSimples;
+import com.stock.stock.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SkuSimplesRepository extends JpaRepository <SkuSimples, Integer> {
     Optional<SkuSimples> findBySKU(String SKU);
+    List<SkuSimples> findAllByUser (User user);
 
 
 
