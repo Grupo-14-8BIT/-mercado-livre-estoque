5ba1662eddb0167e698077a1045e10093627851f
Arthur Henrique Vieira Busanello <tuti.busanello@hotmail.com>
2024-05-07 20:55:29 -0300

    criei o log

--- src/main/java/com/stock/stock/StockApplication.java
+++ src/main/java/com/stock/stock/StockApplication.java
@@ -2,8 +2,9 @@ package com.stock.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
+@Slf4j
 public class StockApplication {
 
 	public static void main(String[] args) {
